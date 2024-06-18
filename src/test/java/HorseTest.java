058904d066ddbfa17a37625019b18aa6ef74235b
Uliana Bila <belaau67@gmail.com>
2024-06-18 17:27:59 +0300

    Added ParameterizedTest constructor_NoProhibitedCharactersInHorseName_ThrowsIllegalArgumentException

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +11,13 @@ class HorseTest {
         Throwable exception = assertThrows(IllegalArgumentException.class, () ->
                 new Horse(null, 5));
         assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"","\t","\n"," "})
+    void constructor_NoProhibitedCharactersInHorseName_ThrowsIllegalArgumentException(String argument){
+        Throwable exception = assertThrows(IllegalArgumentException.class, () ->
+                new Horse(argument, 5));
+        assertEquals("Name cannot be blank.", exception.getMessage());
     }
 }
