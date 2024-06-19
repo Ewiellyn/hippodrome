31aa18acd4b8742de2e5a611fffa66248efbe664
Uliana Bila <belaau67@gmail.com>
2024-06-19 22:22:55 +0300

    Created logger for Hippodrome class and added info methods

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,21 +1,28 @@
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import org.apache.logging.log4j.*;
+
+
 
 import static java.util.Objects.isNull;
 
 public class Hippodrome {
 
     private final List<Horse> horses;
+    private static final Logger hippodromelogger = LogManager.getLogger(Hippodrome.class.getName());
 
     public Hippodrome(List<Horse> horses) {
         if (isNull(horses)) {
+            hippodromelogger.error("Horses list is null");
             throw new IllegalArgumentException("Horses cannot be null.");
         } else if (horses.isEmpty()) {
+            hippodromelogger.error("Horses list is empty");
             throw new IllegalArgumentException("Horses cannot be empty.");
         }
 
         this.horses = horses;
+        hippodromelogger.debug("створення Hippodrome, коней ["+horses.size()+"]");
     }
 
     public List<Horse> getHorses() {
