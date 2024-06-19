f93716c608e395544437926c1efa3cf654ba45a3
Uliana Bila <belaau67@gmail.com>
2024-06-19 10:35:48 +0300

    Added tests for move method:  move_CheckIfGetRandomDoubleWithParametersCalled_true(), move_CheckCorrectnessOfCalculation_true(double argument)

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +15,8 @@ class HorseTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"","\t","\n"," "})
-    void constructor_NoProhibitedCharactersInHorseName_ThrowsIllegalArgumentException(String argument){
+    @ValueSource(strings = {"", "\t", "\n", " "})
+    void constructor_NoProhibitedCharactersInHorseName_ThrowsIllegalArgumentException(String argument) {
         Throwable exception = assertThrows(IllegalArgumentException.class, () ->
                 new Horse(argument, 5));
         assertEquals("Name cannot be blank.", exception.getMessage());
@@ -36,26 +37,46 @@ class HorseTest {
     }
 
     @Test
-    void getName_MethodReturnsName_true(){
-        Horse horse = new Horse("Sam",5);
-        assertEquals("Sam",horse.getName());
+    void getName_MethodReturnsName_true() {
+        Horse horse = new Horse("Sam", 5);
+        assertEquals("Sam", horse.getName());
     }
 
     @Test
-    void getSpeed_MethodReturnsSpeed_true(){
-        Horse horse = new Horse("Sam",5);
-        assertEquals(5,horse.getSpeed());
+    void getSpeed_MethodReturnsSpeed_true() {
+        Horse horse = new Horse("Sam", 5);
+        assertEquals(5, horse.getSpeed());
     }
 
     @Test
-    void getDistance_MethodReturnsDistance_true(){
-        Horse horse = new Horse("Sam",5,10);
-        assertEquals(10,horse.getDistance());
+    void getDistance_MethodReturnsDistance_true() {
+        Horse horse = new Horse("Sam", 5, 10);
+        assertEquals(10, horse.getDistance());
     }
 
     @Test
-    void getDistance_MethodReturnsNullWhenConstructorWithTwoParameters_true(){
-        Horse horse = new Horse("Sam",5);
-        assertEquals(0,horse.getDistance());
+    void getDistance_MethodReturnsNullWhenConstructorWithTwoParameters_true() {
+        Horse horse = new Horse("Sam", 5);
+        assertEquals(0, horse.getDistance());
+    }
+
+    @Test
+    void move_CheckIfGetRandomDoubleWithParametersCalled_true() {
+        try (MockedStatic<Horse> mockedHorse = Mockito.mockStatic(Horse.class)) {
+            Horse testHorse = new Horse("Sam",5);
+            testHorse.move();
+            mockedHorse.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.3,0.4895,0.57})
+    void move_CheckCorrectnessOfCalculation_true(double argument) {
+        try (MockedStatic<Horse> mockedHorse = Mockito.mockStatic(Horse.class)) {
+            Horse testHorse = new Horse("Sam",5,7);
+            mockedHorse.when(() -> Horse.getRandomDouble(0.2,0.9)).thenReturn(argument);
+            testHorse.move();
+            assertEquals(7+5*Horse.getRandomDouble(0.2,0.9),testHorse.getDistance());
+        }
     }
 }
